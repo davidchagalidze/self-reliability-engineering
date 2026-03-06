04f41c2c1f48f65581300f9ffb5874c59ffea893
David Chagalidze <d.chagalidze@strikerz.inc>
2026-03-07 00:35:54 +0400

    now can run the app in ide

--- telemetry-api/app/main.py
+++ telemetry-api/app/main.py
@@ -5,3 +5,14 @@ app = FastAPI(title="SRE Telemetry API")
 
 # Connect the routes from routes.py
 app.include_router(router)
+
+
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run(
+        "app.main:app",
+        host="0.0.0.0",
+        port=8000,
+        reload=True,
+    )
